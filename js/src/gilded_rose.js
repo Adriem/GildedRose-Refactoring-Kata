1809cf46814d06d5a8d3cdcace2ef230612086d4
Adrian Moreno <adrn.moreno@gmail.com>
2018-04-26 19:13:15 +0200

    Fix updateQuality for Backstage passes

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -16,6 +16,7 @@ const updateQualityStrategies = {
     return item.quality + 1;
   },
   'Backstage passes to a TAFKAL80ETC concert': item => {
+    if (item.sellIn < 0) return 0;
     if (item.quality >= MAX_QUALITY) return MAX_QUALITY;
     if (item.sellIn < 6) return Math.min(item.quality + 3, MAX_QUALITY);
     if (item.sellIn < 11) return Math.min(item.quality + 2, MAX_QUALITY);
@@ -63,8 +64,6 @@ class Shop {
               item.quality = item.quality - 1;
             }
           }
-        } else {
-          item.quality = item.quality - item.quality;
         }
       }
     }
